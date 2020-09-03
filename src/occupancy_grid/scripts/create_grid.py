2e7bae909359db40a645f9331c26b9eeec81271e
nivolog <golovin.va@phystech.edu>
2020-09-03 13:36:56 +0300

    Added ros parameters support

--- src/occupancy_grid/scripts/create_grid.py
+++ src/occupancy_grid/scripts/create_grid.py
@@ -4,39 +4,39 @@ from std_msgs.msg import String
 from nav_msgs.msg import OccupancyGrid
 import numpy as np
 
-path = rospy.get_param('~path', '/home/vlad/test/1')
-path += '/grid.txt'
-
 grid = OccupancyGrid()
-pub = rospy.Publisher('set_occupancy_grid', OccupancyGrid, queue_size = 5)
+pub = rospy.Publisher('set_occupancy_grid', OccupancyGrid, queue_size=5)
+
 
 def MapCreator():
-    rospy.init_node('occupancy_grid', anonymous = False)
+    rospy.init_node('occupancy_grid', anonymous=False)
     sub = rospy.Subscriber('action', String, callback)
-    #TODO: mission action
+    # TODO: mission action
 
     rospy.spin()
+
 
 def callback(action):
     if action.data == 'read-grid' or action.data == 'read-data':
         rospy.loginfo_once("reading grid")
         read_grid()
     elif action.data == 'set-grid' or action.data == 'set-data':
-        pub.publish(grid);
+        pub.publish(grid)
         rospy.loginfo_once("publishing grid")
     elif action.data == 'load-data':
         rospy.loginfo_once("Reading and publishing grid")
         read_grid()
         pub.publish(grid)
 
+
 def read_grid():
     with open(path) as mapData:
         grid.info.width = np.uint32(mapData.readline())
         grid.info.height = np.uint32(mapData.readline())
-        grid.info.resolution = np.float32(mapData.readline());
-        grid.info.origin.position.x = np.float64(mapData.readline());
-        grid.info.origin.position.y = np.float64(mapData.readline());
-        grid.info.origin.position.z = np.float64(mapData.readline());
+        grid.info.resolution = np.float32(mapData.readline())
+        grid.info.origin.position.x = np.float64(mapData.readline())
+        grid.info.origin.position.y = np.float64(mapData.readline())
+        grid.info.origin.position.z = np.float64(mapData.readline())
         data = []
         while True:
             c = mapData.readline()
@@ -45,5 +45,8 @@ def read_grid():
             data.append(np.int8(c))
         grid.data = data
 
+
 if __name__ == '__main__':
+    path = rospy.get_param("data_folder")
+    path = path + '/grid.txt'
     MapCreator()
