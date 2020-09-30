8f9abf1691d134e5e5fb0c05dc0ec8708072a07c
nivolog <golovin.va@phystech.edu>
2020-09-30 16:24:46 +0300

    Added file that performs high logic

--- src/planner_cmc/src/astar/planner.cpp
+++ src/planner_cmc/src/astar/planner.cpp
@@ -24,19 +24,17 @@
  * breakingTies - defines order of opening the nodes when f-value is equal: g-min(false) or g-max(true)
  */
 
+
+//todo: you may override this func to implement your algorithm
 void search(nav_msgs::OccupancyGrid &grid, geometry_msgs::Point& startPoint, geometry_msgs::Point& goalPoint, rtp_msgs::PathStamped& path){
     ROS_INFO_STREAM("starting");
     A_search seeker = A_search(1.0, euclidean, true, true, true, true);
-    //ROS_INFO_STREAM("log point");
     Map occupancyMap = Map(grid);
-    //ROS_INFO_STREAM("log point");
     rtp_msgs::PathStamped pathMsg;
     Node start(startPoint.x, startPoint.y, grid.info.height*startPoint.y + startPoint.x);
     Node goal(goalPoint.x, goalPoint.y, grid.info.height*goalPoint.y + goalPoint.x);
-    //ROS_INFO_STREAM("data loaded");
-    //seeker.search(start, goal, occupancyMap);
+    seeker.search(start, goal, occupancyMap);
     seeker.create_path(startPoint, pathMsg);
-    //seeker.extract_path(tmp);//todo: need to override this func
     seeker.clear();
     ROS_INFO_STREAM("end of planning");
     ROS_INFO_STREAM(pathMsg);
